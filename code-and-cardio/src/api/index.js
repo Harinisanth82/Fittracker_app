c233883878f9151b38ab209e7fb2655e1e5f2824
Harinisanth82 <harinisanth563@gmail.com>
2025-04-16 11:52:49 +0530

    API Added

--- code-and-cardio/src/api/index.js
+++ code-and-cardio/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API=axios.create({
-    baseURL: "http://localhost:8080/api",
+  baseURL: "https://fittracker-app.onrender.com/api/",
 });
 export const userSignup=async(data)=>API.post("/user/signup",data);
 export const userSignin=async(data)=>API.post("/user/signin",data);
